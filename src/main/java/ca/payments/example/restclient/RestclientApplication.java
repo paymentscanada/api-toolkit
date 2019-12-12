49fba20987315942cf6250e67fef6f69ae2c350b
adaccache <adaccache@payments.ca>
2019-12-12 17:21:17 -0500

    initial checkin

---
+++ src/main/java/ca/payments/example/restclient/RestclientApplication.java
@@ -0,0 +1,13 @@
+package ca.payments.example.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/payments/example/restclient/RestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.payments.example.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
