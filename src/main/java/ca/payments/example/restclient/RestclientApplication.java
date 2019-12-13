d550f3f200c66b77c14d306a15511fb7d7a3c637
adaccache <adaccache@payments.ca>
2019-12-13 18:34:37 -0500

    working rest client

--- src/main/java/ca/payments/example/restclient/RestclientApplication.java
+++ src/main/java/ca/payments/example/restclient/RestclientApplication.java
@@ -8,29 +8,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class RestclientApplication {
 
-
 	private static final Logger log = LoggerFactory.getLogger(RestclientApplication.class);
+	private static MediaType fifMediaType = new MediaType("application", "vnd.fif.api.v1+json");
+	private static String gateway_auth_url = "https://api.payments.ca/accesstoken";
+	private static String fif_branch_url = "https://api.payments.ca/fif-branch-sandbox/branches/";
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestclientApplication.class, args);
 	}
 
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+	public CommandLineRunner run()  {
 		return args -> {
-			ExampleResponseModel erm = restTemplate.getForObject(
-					"https://gturnquist-quoters.cfapps.io/api/random", ExampleResponseModel.class);
-			log.info(erm.toString());
+
+			String url = fif_branch_url + "123456789";
+			String bearer = "BEARER_TOKEN";
+
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+
+			headers.setAccept(Collections.singletonList(fifMediaType));
+			headers.setBearerAuth(bearer);
+
+			HttpEntity<?> entity = new HttpEntity<>(headers);
+
+			try {
+				ResponseEntity<ExampleResponseModel> respEntity = restTemplate.exchange(url, HttpMethod.GET, entity, ExampleResponseModel.class);
+				log.info(respEntity.toString());
+			}catch (Exception ex){
+				log.error("Exception caught: ", ex);
+			}
+
 		};
 	}
 
